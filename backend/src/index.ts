4cf4334239024a9c4439a1358c9c04769e3c9785
Haider Ali <haideralyy01@gmail.com>
2026-03-16 21:25:33 +0530

    feat: add/see content routes add

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import { ContentModel, LinkModel, UserModel } from './db.js';
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from './config.js';
 import { userMiddleware } from './middleware.js';
-
+import { random } from './utils.js';
 
 const app = express();
 app.use(express.json());
@@ -56,6 +56,38 @@ app.post("/api/v1/signin", async (req, res) => {
     } catch (e) {
         console.error("Error during signin:", e);
         res.status(500).json({ msg: "Internal server error" });
+    }
+});
+
+app.post("/api/v1/content", userMiddleware, async (req, res) => {
+    const {title, link} = req.body;
+    if (!title || !link) {
+        return res.status(400).json({ message: "Title and link are required" });
+    }
+    try{
+        const content = await ContentModel.create({
+            userId: new mongoose.Types.ObjectId(req.userId),
+            title,
+            link,
+            tags: [],
+        });
+        res.status(200).json({ message: "Content created successfully", content });
+    } catch (e) {
+        console.error("Error creating content:", e);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+app.get("/api/v1/contents", userMiddleware, async (req, res) => {
+    const UserId = req.userId;
+    try {
+        const content = await ContentModel.find({ userId:  new mongoose.Types.ObjectId(UserId) }).populate('userId', '-password');
+        res.status(200).json({
+            content
+        })
+    } catch (e) {
+        console.error("Error fetching content:", e);
+        res.status(500).json({ message: "Internal server error" });
     }
 });
 
