b62e0b98b978c8931dd0cb25d91d3c4de6f1e590
Haider Ali <haideralyy01@gmail.com>
2026-03-16 21:17:55 +0530

    db schema designed

---
+++ backend/src/db.ts
@@ -0,0 +1,43 @@
+
+import mongoose, { model, Schema } from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+(async () => {
+    try {
+        const dbUri = process.env.DB_CONNECTION_STRING;
+        if (!dbUri) {
+            throw new Error('DB_CONNECTION_STRING not set in .env');
+        }
+        await mongoose.connect(dbUri);
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.error("Database connection error:", error);
+        process.exit(1);
+    }
+})();
+
+const UserSchema = new Schema({
+    username: { type: String, unique: true },
+    password: String
+});
+
+const ContentSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    title: String,
+    link: String,
+    tags: [{type: Schema.Types.ObjectId, ref: 'Tags'}]
+});
+
+const LinkSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true, unique: true },
+    hash: String,
+});
+
+const ContentModel = model('Content', ContentSchema)
+const UserModel = model('User', UserSchema);
+const LinkModel = model('Link', LinkSchema);
+
+export { UserModel, ContentModel, LinkModel }
+
